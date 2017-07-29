a05669aacad2704ca92930a7b0027d4520e4c840
tnwls654 <tnwls654>
2017-07-30 07:16:34 +0900

    checklist2 xml

--- app/src/main/java/pnugirls/evergreen/MainActivity.java
+++ app/src/main/java/pnugirls/evergreen/MainActivity.java
@@ -1,18 +1,34 @@
 package pnugirls.evergreen;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import static android.R.attr.button;
 
-    Button botton;
+public class MainActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
+        Button b =(Button)findViewById(R.id.checklist1);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        getApplicationContext(),checkListActivity.class);
+                startActivity(intent);
+            }
+        });
+        } //end onCreate()
 
 
-}
+    } //end MainActivity
+
+
+
