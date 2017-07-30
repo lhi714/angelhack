a01b342e4fc468b61b22c4551ea69531cfa0ef87
tnwls654 <tnwls654>
2017-07-30 09:24:51 +0900

    Merge branch 'master' of https://github.com/lhi714/angelhack into soojinBranch
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/pnugirls/evergreen/checkListActivity.java
+++ app/src/main/java/pnugirls/evergreen/checkListActivity.java
@@ -3,6 +3,8 @@ package pnugirls.evergreen;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by doohee on 2017-07-30.
@@ -15,7 +17,16 @@ public class checkListActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checklist1);
 
-       // Intent intent = new Intent(this, checkListActivity.class);
-       // startActivity(intent);
-    }
-}
+        Button b2 =(Button)findViewById(R.id.btnNExt);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        getApplicationContext(),checkListActivity2.class);
+                startActivity(intent);
+            }
+        });
+    } //end onCreate()
+
+
+} //end MainActivity
---
+++ app/src/main/java/pnugirls/evergreen/checkListActivity2.java
@@ -0,0 +1,19 @@
+package pnugirls.evergreen;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by doohee on 2017-07-30.
+ */
+
+public class checkListActivity2 extends Activity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checklist2);
+    }
+
+    }
