22e35d684892cd86e37d32b3d16bc38b228e0b65
EUNJU <cokxz12@gmail.com>
2017-07-29 21:30:18 -0700

    ....후

--- app/src/main/java/pnugirls/evergreen/CustomListActivity.java
+++ app/src/main/java/pnugirls/evergreen/CustomListActivity.java
@@ -1,22 +1,26 @@
 package pnugirls.evergreen;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class CustomListActivity extends AppCompatActivity {
 
-    private 
-    private Button btnadd;
+    private PopupWindow popupWindow;
+    private Button btnadd, btnorder;
     private CustomListAdapter adapter;
     public ArrayList<CustomListItem> clitems;
 
@@ -27,6 +31,7 @@ public class CustomListActivity extends AppCompatActivity {
 
         clitems = new ArrayList<CustomListItem>();
         btnadd = (Button)findViewById(R.id.btnadd);
+        btnorder = (Button)findViewById(R.id.btnorder);
 
         for(int i=0; i<9; i++) {
 
@@ -46,14 +51,15 @@ public class CustomListActivity extends AppCompatActivity {
         listview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getApplicationContext(), "Delete"+clitems.get(i).getName(),Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Delete "+clitems.get(i).getName(),Toast.LENGTH_SHORT).show();
                 clitems.remove(i);
                 adapter.notifyDataSetChanged();
                 return false;
             }
         });
-        btnadd.setOnClickListener(new View.OnClickListener() {
-            @Override
+
+       btnadd.setOnClickListener(new View.OnClickListener() {
+            @Override//
             public void onClick(View view) {
                Intent intent=new Intent(getApplicationContext(), AddPopupWindow.class);
                startActivityForResult(intent,0);
@@ -62,23 +68,22 @@ public class CustomListActivity extends AppCompatActivity {
 
     }
 
-    public void onActivityResult(int requestCode, int resultCode, Intent intent){
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
 
         super.onActivityResult(requestCode, resultCode, intent);
-        switch(requestCode){
+        switch (requestCode) {
             case 0:
-                if(resultCode == RESULT_OK){
-                    ArrayList<CustomListItem> it= intent.getParcelableExtra("Select");
-                    for(int i=0; i<it.size();i++){
-                        clitems.add(it.get(i));
-                        adapter.notifyDataSetChanged();
-                    }
+                if (resultCode == RESULT_OK) {
+                    CustomListItem clitems1 = new CustomListItem();
+                    clitems1.setIcon(intent.getIntExtra("Select_Icon", 0));
+                    clitems1.setName(intent.getStringExtra("Select_Name"));
+                    clitems1.setContents(intent.getStringExtra("Select_Content"));
+                    clitems.add(clitems1);
+                    adapter.notifyDataSetChanged();
                 }
-
+               break;
         }
+    }
 
-    }
-    public void AddCustomArray(CustomListItem item){
-        clitems.add(item);
-    }
+
 }
