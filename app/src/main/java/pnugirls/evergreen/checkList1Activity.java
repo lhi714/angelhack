ccfe8cedb33316270425dc0603f5c63fcd4255b3
HyeinLee <gpdls714@gmail.com>
2017-07-30 15:15:00 +0900

    checklist page moving

--- app/src/main/java/pnugirls/evergreen/checkList1Activity.java
+++ app/src/main/java/pnugirls/evergreen/checkList1Activity.java
@@ -4,29 +4,54 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+
+import static pnugirls.evergreen.R.id.nextButton1;
 
 /**
  * Created by doohee on 2017-07-30.
  */
 
-public class checkListActivity extends Activity {
+public class checkList1Activity extends Activity {
+    private final int MALE = 1;
+    private final int FEMALE = 2;
+
+    private int gender = 1;
+
+    public static Activity checkList1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checklist1);
 
-        Button nextButton = (Button) findViewById(R.id.nextButton);
-        Button previousButton = (Button) findViewById(R.id.previousButton);
+        ImageButton nextButton = (ImageButton) findViewById(nextButton1);
 
-        previousButton.setOnClickListener(new View.OnClickListener() {
+        nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(this, checkListActivity.class))
+                startActivity(new Intent(checkList1Activity.this, checkList2Activity.class));
             }
         });
-       // Intent intent = new Intent(this, checkListActivity.class);
-       // startActivity(intent);
+
+        checkList1 = checkList1Activity.this;
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.male:
+                if (checked)
+                    gender = MALE;
+                    break;
+            case R.id.female:
+                if (checked)
+                    gender = FEMALE;
+                    break;
+        }
     }
 }
