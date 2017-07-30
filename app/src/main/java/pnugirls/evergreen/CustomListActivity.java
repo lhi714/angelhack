3320957c402ec68cb76db228c6b039c28554871d
EUNJU <cokxz12@gmail.com>
2017-07-29 20:34:03 -0700

    ....후

--- app/src/main/java/pnugirls/evergreen/CustomListActivity.java
+++ app/src/main/java/pnugirls/evergreen/CustomListActivity.java
@@ -1,21 +1,24 @@
 package pnugirls.evergreen;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class CustomListActivity extends AppCompatActivity {
 
+    private 
     private Button btnadd;
-    private ArrayList<Integer> cint;
-    private ArrayList<CustomListItem> clitems;
-    public int canclecnt=0;
+    private CustomListAdapter adapter;
+    public ArrayList<CustomListItem> clitems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +26,6 @@ public class CustomListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_custom_list);
 
         clitems = new ArrayList<CustomListItem>();
-        cint = new ArrayList<Integer>();
         btnadd = (Button)findViewById(R.id.btnadd);
 
         for(int i=0; i<9; i++) {
@@ -36,42 +38,47 @@ public class CustomListActivity extends AppCompatActivity {
             clitems.add(clitems1);
         }
 
-        final CustomListAdapter adapter=new CustomListAdapter(this, clitems);
+        adapter=new CustomListAdapter(this, clitems);
 
         final ListView listview =(ListView)findViewById(R.id.clistview);
         listview.setAdapter(adapter);
 
-
-        listview.setOnItemClickListener(
-                new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> adapterView, View view, int index, long l) {
-                        if(adapter.
-                            cint.add(index);
-                            canclecnt++;
-                        }
-                        if(canclecnt!=0) btnadd.setText("Delete");
-                    }
-                }
-        );
-
+        listview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), "Delete"+clitems.get(i).getName(),Toast.LENGTH_SHORT).show();
+                clitems.remove(i);
+                adapter.notifyDataSetChanged();
+                return false;
+            }
+        });
         btnadd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(btnadd.getText()=="Add"){}
-                else{
-                    if(cint.size()>0){
-                        for(int i=0; i<cint.size(); i++) {
-                            clitems.remove(cint.get(i));
-                            listview.clearChoices();
-                            adapter.notifyDataSetChanged();
-                        }
-                        cint.clear();
-                    }
-                }
+               Intent intent=new Intent(getApplicationContext(), AddPopupWindow.class);
+               startActivityForResult(intent,0);
             }
         });
 
+    }
 
+    public void onActivityResult(int requestCode, int resultCode, Intent intent){
+
+        super.onActivityResult(requestCode, resultCode, intent);
+        switch(requestCode){
+            case 0:
+                if(resultCode == RESULT_OK){
+                    ArrayList<CustomListItem> it= intent.getParcelableExtra("Select");
+                    for(int i=0; i<it.size();i++){
+                        clitems.add(it.get(i));
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+
+        }
+
+    }
+    public void AddCustomArray(CustomListItem item){
+        clitems.add(item);
     }
 }
