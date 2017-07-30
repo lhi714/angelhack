5c82f707121ecd23001b888274d33f55b4e01f72
EUNJU <cokxz12@gmail.com>
2017-07-30 08:14:27 -0700

    ....후

--- app/src/main/java/pnugirls/evergreen/CustomListActivity.java
+++ app/src/main/java/pnugirls/evergreen/CustomListActivity.java
@@ -15,38 +15,76 @@ import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 
 public class CustomListActivity extends AppCompatActivity {
 
-    private PopupWindow popupWindow;
+
     private Button btnadd, btnorder;
     private CustomListAdapter adapter;
     public ArrayList<CustomListItem> clitems;
-
+    public int[] exm={0,0,1,0,0,0,0,1,1,0,1,0,1,0};
+    public boolean calledAlready = false;
+    ChooseIngredient chooseIngredient;
+    private String[] Indigrent={"apple","canola","celery","kale","broccoli",
+        "tomato","carrot","pineapple","pomegranate" ,"banana",
+        "japanese plum","cucumber","houttuynia cordata","water parsely","lemon",
+        "aloe","raspberry","persimmon","plum","parsely",
+        "crown daisy","chive","tangerine","cabbage","cornus fruit"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(this);
         setContentView(R.layout.activity_custom_list);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance(Config.FIREBASE_URL);
+        DatabaseReference databaseReference = database.getReference("Vegi");
+
+        chooseIngredient=new ChooseIngredient();
+        final ArrayList<Integer> result=chooseIngredient.GetIngrediChoose(exm);
 
         clitems = new ArrayList<CustomListItem>();
         btnadd = (Button)findViewById(R.id.btnadd);
         btnorder = (Button)findViewById(R.id.btnorder);
 
-        for(int i=0; i<9; i++) {
 
-            CustomListItem clitems1 = new CustomListItem();
-            clitems1.setIcon(R.drawable.background_login);
-            clitems1.setName("Salary");
-            clitems1.setContents("hohohohohohohoohhohohohohoh");
-
-            clitems.add(clitems1);
-        }
 
         adapter=new CustomListAdapter(this, clitems);
-
         final ListView listview =(ListView)findViewById(R.id.clistview);
         listview.setAdapter(adapter);
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(int i=0; i<4; i++) {
+                    for (DataSnapshot messageData : dataSnapshot.getChildren()) {
+                        if (Indigrent[result.get(i)] == messageData.getKey().toString()) {
+                            CustomListItem clitems1 = new CustomListItem();
+                            clitems1.setIcon(R.drawable.ic_launcher_round);
+                            clitems1.setName(messageData.getKey().toString());
+                            clitems1.setContents(messageData.getValue().toString());
+                            clitems.add(clitems1);
+                        }
+                    }
+                }
+                    adapter.notifyDataSetChanged();
+                    listview.setSelection(adapter.getCount() - 1);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
 
         listview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
